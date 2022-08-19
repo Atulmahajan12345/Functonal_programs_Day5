fca1aa2fa111f8daa1a146e0102828f83196454e
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-08-19 16:32:54 +0530

    Added:program to find Euclidean distance

---
+++ src/com/bridgelabz/Distance.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Distance {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the value of X");
+		int x = scanner.nextInt();
+		System.out.println("Enter the value of Y");
+		int y = scanner.nextInt();
+		scanner.close();
+		double Distance = Math.sqrt((Math.pow(x, 2))+(Math.pow(y, 2)));
+		System.out.println("Distance is "+ Distance);
+	}
+}
+
+
